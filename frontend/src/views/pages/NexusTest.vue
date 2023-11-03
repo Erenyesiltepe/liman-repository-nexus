fb717b8ff6257ea7bf7ad9f6842a5651f6324686
ErenYesiltepe <eren.bilkentk15@hotmail.com>
2023-11-03 13:30:15 +0000

    nexus added

---
+++ frontend/src/views/pages/NexusTest.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useNexusStore } from "@/stores/nexus"
+import { ref } from "vue"
+import Table from "@/components/Table.vue"
+//import { useI18n } from 'vue-i18n'
+//const { t } = useI18n()
+
+const store = useNexusStore()
+const loading = ref(false)
+
+store.fetchRepositories()
+const test = ref(store.getRepositories)
+console.log(test.value[0])
+
+const columns = ref([
+  {
+    title: "Name",
+    key: "name",
+    sorter: "default",
+    filterable: true,
+  },
+  {
+    title: "Format",
+    key: "format",
+  },
+  {
+    title: "Type",
+    key: "type",
+  },
+  {
+    title: "Url",
+    key: "url",
+  },
+])
+</script>
+<template>
+  <Table :columns="columns" :data="store.getRepositories" :loading="loading">
+  </Table>
+</template>
